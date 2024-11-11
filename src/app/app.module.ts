d3df16c3902a8397502c2bb671f2a49717031a32
Eduardo Moura <eduardo.moura@mvpsolutions.tech>
2024-11-11 14:58:23 -0300

    config(ci e backend) incluindo --force no CI e removendo projeto backend/functions e authentications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AngularFireModule } from '@angular/fire/compat';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -12,20 +12,19 @@ import { AppRoutingModule } from './app-routing.module';
 // material
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatDialogModule } from '@angular/material/dialog';
 
 import { environment } from 'src/environments/environment';
 import { AppComponent } from './app.component';
 import { RegisterVehicleComponent } from './components/register-vehicle/register-vehicle.component';
 import { VehiclesComponent } from './components/vehicles/vehicles.component';
-import { FirebaseAppCheckInterceptor } from './auth/interceptors/firebase-app-check.interceptor';
 import { ViewVehicleComponent } from './components/view-vehicle/view-vehicle.component';
 import { DeleteDialogComponent } from './shared/components/delete-dialog/delete-dialog.component';
 import { VehicleInputsComponent } from './shared/components/vehicle-inputs/vehicle-inputs.component';
@@ -61,12 +60,7 @@ import { VehicleInputsComponent } from './shared/components/vehicle-inputs/vehic
     NgxMaskPipe
   ],
   providers: [
-    provideNgxMask(),
-    // {
-    //   provide: HTTP_INTERCEPTORS,
-    //   useClass: FirebaseAppCheckInterceptor,
-    //   multi: true,
-    // },
+    provideNgxMask()
   ],
   bootstrap: [AppComponent]
 })
