9aa04c344b6343e3b6c588d7d260b1949d5fa942
Eduardo Moura <eduardo.moura@mvpsolutions.tech>
2024-11-10 22:55:08 -0300

    feat(register-vehicle) cadastro e validações de inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
+import { AppRoutingModule } from './app-routing.module';
+
+// material
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AppComponent } from './app.component';
+import { RegisterVehicleComponent } from './components/register-vehicle/register-vehicle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterVehicleComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatProgressSpinnerModule,
+    MatListModule,
+    MatSnackBarModule,
+    NgxMaskDirective,
+    NgxMaskPipe
   ],
-  providers: [],
+  providers: [
+    provideNgxMask(),
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/services/snack-message.service.ts
@@ -0,0 +1,37 @@
+import { Inject, Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SnackMessageService {
+  errorCurrency!: string;
+  noSchedulingObtained!: string;
+  reversalRegistered!: string;
+  registered!: string;
+  updated!: string;
+  deleted!: string;
+  selectedItem!: string;
+  unauthorized!: string;
+  err!: string;
+  actClose!: string;
+
+  constructor(
+    @Inject(MatSnackBar) private readonly snackBar: MatSnackBar,
+  ) { }
+
+  alert(msg: string) {
+    this.snackBar.open(msg, 'fechar', {
+      duration: 3000,
+      panelClass: ['mat-accent'],
+    });
+  }
+
+  error(msg: string) {
+    this.snackBar.open(msg, 'fechar', {
+      duration: 3000,
+      panelClass: ['mat-warn'],
+    });
+  }
+
+}
